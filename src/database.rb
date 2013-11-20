5ed85e2eaf3f120496d39995b8ef703069189d79
Jason Sun <sunapi386@gmail.com>
2013-11-20 03:26:00 -0500

    dual counters

--- src/database.rb
+++ src/database.rb
@@ -76,6 +76,22 @@ class DatabaseBot
     query = "MATCH (n:#{hero})-[r]->(m) WHERE r.advantage < 0 RETURN m.name, r.advantage ORDER BY r.advantage"
     @neo.execute_query(query)['data']
   end
+
+  def dual_counters(hero1, hero2)
+    # Probably a better way is to make a good query and let the graph db figure this out
+    # Finds common heros that counters hero1 and hero2, and sums disadvantage
+    duals = []
+    hero1_counters = what_counters(hero1)
+    hero2_counters = what_counters(hero2)
+    hero2_counters_str = hero2_counters.to_s
+    hero1_counters.each do |hero, advantage|
+      if hero2_counters_str.include? hero
+        duals << [hero, (advantage + hero2_counters.select{ |h, a| h == hero }[0][1]).round(2)]
+      end
+    end
+    duals.sort_by! {|hero,advantage| advantage}
+  end
+
 end
 
 
--- src/test.rb
+++ src/test.rb
@@ -5,4 +5,5 @@ database_bot = DatabaseBot.new
 
 #database_bot.create_graph_db
 #puts database_bot.advantage('lina','lion')
-#database_bot.what_counters('lion').each { |hero, advantage| puts "#{hero} #{advantage}"}
+#database_bot.what_counters('lina').each { |hero, advantage| puts "#{hero} #{advantage}"}
+database_bot.dual_counters('lina', 'lion').each {|hero, advantage| puts "#{hero} #{advantage}"}
