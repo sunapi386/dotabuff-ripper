75c14a361cdca85d9501060bec929621d7162548
Jason Sun <sunapi386@gmail.com>
2013-11-21 02:47:38 -0500

    the real deal! dota cm mode picker

---
+++ src/picker.rb
@@ -0,0 +1,92 @@
+require_relative 'database'
+require 'highline/import'
+
+def sanitized_prompt(input)
+  input.gsub(/[^a-z]+/i, '').downcase
+end
+
+def print_n_counters(n, hero_advantage_list)
+  puts '-----------------------'
+  hero_advantage_list[0..n].each do |hero, advantage|
+    puts "#{hero} #{advantage}"
+  end
+  puts '-----------------------'
+end
+
+
+database_bot = DatabaseBot.new
+
+# Lots of copy pasting code here, to-do: rework pasta
+
+hero1 = sanitized_prompt ask 'Hero 1 >>> '
+hero1_counters = database_bot.what_counters(hero1)
+while hero1_counters.empty?
+  puts "Unrecognized hero '#{hero1}', similar names are..."
+  puts database_bot.search_amatch hero1
+  puts  '...'
+  hero1 = sanitized_prompt ask 'Please re-enter Hero 1 >>> '
+  hero1_counters = database_bot.what_counters(hero1)
+end
+puts "Counters to #{hero1}:"
+print_n_counters 10, hero1_counters
+
+
+hero2 = sanitized_prompt ask 'Hero 2 >>> '
+hero2_counters = database_bot.what_counters hero2
+while hero2_counters.empty?
+  puts "Unrecognized hero '#{hero2}', similar names are..."
+  puts database_bot.search_amatch hero2
+  puts  '...'
+  hero2 = sanitized_prompt ask 'Please re-enter Hero 2 >>> '
+  hero2_counters = database_bot.what_counters(hero2)
+end
+two_counters = database_bot.merge_counters hero1_counters, hero2_counters
+puts "Counters to #{hero1}, and #{hero2}:"
+print_n_counters 10, two_counters
+
+
+hero3 = sanitized_prompt ask 'Hero 3 >>> '
+hero3_counters = database_bot.what_counters hero3
+while hero3_counters.empty?
+  puts "Unrecognized hero '#{hero3}', similar names are..."
+  puts database_bot.search_amatch hero3
+  puts  '...'
+  hero3 = sanitized_prompt ask 'Please re-enter Hero 3 >>> '
+  hero3_counters = database_bot.what_counters(hero3)
+end
+three_counters = database_bot.merge_counters two_counters, hero3_counters
+puts "Counters to #{hero1}, #{hero2}, and #{hero3}:"
+print_n_counters 10, three_counters
+
+
+hero4 = sanitized_prompt ask 'Hero 4 >>> '
+hero4_counters = database_bot.what_counters hero4
+while hero4_counters.empty?
+  puts "Unrecognized hero '#{hero4}', similar names are..."
+  puts database_bot.search_amatch hero4
+  puts  '...'
+  hero4 = sanitized_prompt ask 'Please re-enter Hero 4 >>> '
+  hero4_counters = database_bot.what_counters(hero4)
+end
+four_counters = database_bot.merge_counters three_counters, hero4_counters
+puts "Counters to #{hero1}, #{hero2}, #{hero3}, and #{hero4}:"
+print_n_counters 10, four_counters
+
+
+hero5 = sanitized_prompt ask 'Hero 5 >>> '
+hero5_counters = database_bot.what_counters hero5
+while hero5_counters.empty?
+  puts "Unrecognized hero '#{hero5}', similar names are..."
+  puts database_bot.search_amatch hero5
+  puts  '...'
+  hero5 = sanitized_prompt ask 'Please re-enter Hero 5 >>> '
+  hero5_counters = database_bot.what_counters(hero5)
+end
+five_counters = database_bot.merge_counters four_counters, hero5_counters
+puts "Counters to #{hero1}, #{hero2}, #{hero3}, #{hero4}, and #{hero5}:"
+print_n_counters 10, five_counters
+puts 'Done'
+
+
+
+
